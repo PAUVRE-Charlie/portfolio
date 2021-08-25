2e426ac6c307725589b1da90cc224b828f701549
Charlie <front-end@wolfox.co>
2021-08-26 00:55:11 +0200

    add icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,11 @@
-import Logo from './android-chrome-192x192.png'
+import logo from './android-chrome-192x192.png'
+import web from './web.svg'
 
-export { Logo }
+import github_dark from './social_media/Github_dark.svg'
+import github_medium from './social_media/Github_medium.svg'
+import linkedin from './social_media/Linkedin.svg'
+
+export {
+    logo, web,
+    github_dark, github_medium, linkedin
+}
