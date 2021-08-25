be4a97901f39b1e3ceb13629e5ff2b62423f86e2
Charlie <front-end@wolfox.co>
2021-08-26 01:01:49 +0200

    add tgeu project images

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import deerEating from './deer/eating.png'
+import deerHappy from './deer/happy.png'
+import deerHello from './deer/hello.png'
+
+
+import shape1 from './skills/shape1.png'
+import shape2 from './skills/shape2.png'
+import shape3 from './skills/shape3.png'
+import skill1 from './skills/skill1.png'
+import skill2 from './skills/skill2.png'
+import skill3 from './skills/skill3.png'
+
+export { deerEating, deerHappy, deerHello,
+        shape1, shape2, shape3, skill1, skill2, skill3,
+}
